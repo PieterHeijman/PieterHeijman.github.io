8eea8fa8fec7b90beed25a37551d48e16589c376
Pieter Heijman <pieterheijman@outlook.com>
2022-01-18 00:33:05 +0100

    [TASK] Social sharer urls

--- assets/src/modules/SocialShare.tsx
+++ assets/src/modules/SocialShare.tsx
@@ -4,9 +4,25 @@ import * as Popover from '@radix-ui/react-popover';
 
 export default class SocialShare {
   hook: HTMLElement;
+  title: string;
+  url: string;
+  image: string;
+  twitter: string;
+  facebook: string;
+  pinterest: string;
+  linkedin: string;
+  reddit: string;
 
   constructor(hook: HTMLElement) {
     this.hook = hook;
+    this.title = this.hook.getAttribute('data-title');
+    this.url = this.hook.getAttribute('data-url');  
+    this.image = this.hook.getAttribute('data-image');  
+    this.twitter = `https://twitter.com/share?url=https://www.trivialdiscourse.com${this.url}&text=${this.title}`;
+    this.facebook = `https://www.facebook.com/sharer.php?u=https://www.trivialdiscourse.com${this.url}`;
+    this.pinterest = `https://pinterest.com/pin/create/bookmarklet/?media=https://www.trivialdiscourse.com${this.image}&url=https://www.trivialdiscourse.com${this.url}&description=${this.title}`;
+    this.linkedin = `https://www.linkedin.com/shareArticle?url=https://www.trivialdiscourse.com${this.url}&title=${this.title}`;
+    this.reddit = `https://reddit.com/submit?url=https://www.trivialdiscourse.com${this.url}&title=${this.title}`;
   }
 
   render() {
@@ -19,7 +35,7 @@ export default class SocialShare {
             <Popover.Content side="top" sideOffset={8} className="social-share-content">
               <ul className="links">
                 <li>
-                  <a href="https://twitter.com/share?url=https://www.trivialdiscourse.com{{ page.url }}&text={{ page.title }}" target="_blank" aria-label="Share this on Twitter" rel="noopener noreferrer">
+                  <a href={this.twitter} target="_blank" aria-label="Share this on Twitter" rel="noopener noreferrer">
                     <svg xmlns="http://www.w3.org/2000/svg"
                       aria-label="Twitter" role="img"
                       viewBox="0 0 512 512"><rect
@@ -27,7 +43,7 @@ export default class SocialShare {
                       rx="15%"
                       fill="#1da1f2"/><path fill="#fff" d="M437 152a72 72 0 01-40 12a72 72 0 0032-40a72 72 0 01-45 17a72 72 0 00-122 65a200 200 0 01-145-74a72 72 0 0022 94a72 72 0 01-32-7a72 72 0 0056 69a72 72 0 01-32 1a72 72 0 0067 50a200 200 0 01-105 29a200 200 0 00309-179a200 200 0 0035-37"/></svg>
                   </a>
-                  <a href="https://www.facebook.com/sharer.php?u=https://www.trivialdiscourse.com{{ page.url }}" target="_blank" aria-label="Share this on Facebook" rel="noopener noreferrer">
+                  <a href={this.facebook} target="_blank" aria-label="Share this on Facebook" rel="noopener noreferrer">
                     <svg xmlns="http://www.w3.org/2000/svg"
                       aria-label="Facebook" role="img"
                       viewBox="0 0 512 512"><rect
@@ -35,7 +51,7 @@ export default class SocialShare {
                       rx="15%"
                       fill="#1877f2"/><path d="M355.6 330l11.4-74h-71v-48c0-20.2 9.9-40 41.7-40H370v-63s-29.3-5-57.3-5c-58.5 0-96.7 35.4-96.7 99.6V256h-65v74h65v182h80V330h59.6z" fill="#fff"/></svg>
                   </a>
-                  <a href="https://pinterest.com/pin/create/bookmarklet/?media=https://www.trivialdiscourse.com{{ page.image }}&url=https://www.trivialdiscourse.com{{ page.url }}&description={{ page.title }}" target="_blank" aria-label="Share this on Pinterest" rel="noopener noreferrer">
+                  <a href={this.pinterest} target="_blank" aria-label="Share this on Pinterest" rel="noopener noreferrer">
                     <svg xmlns="http://www.w3.org/2000/svg"
                       aria-label="Pinterest" role="img"
                       viewBox="0 0 512 512"><rect
@@ -43,7 +59,7 @@ export default class SocialShare {
                       rx="15%"
                       fill="#bd081b"/><path d="m265 65c-104 0-157 75-157 138 0 37 14 71 45 83 5 2 10 0 12-5l3-18c2-6 1-7-2-12-9-11-15-24-15-43 0-56 41-106 108-106 60 0 92 37 92 85 0 64-28 116-70 116-23 0-40-18-34-42 6-27 19-57 19-77 0-18-9-34-30-34-24 0-42 25-42 58 0 20 7 34 7 34l-29 120a249 249 0 0 0 2 86l3-1c2-3 31-37 40-72l16-61c7 15 29 28 53 28 71 0 119-64 119-151 0-66-56-126-140-126z" fill="#fff"/></svg>
                   </a>
-                  <a href="https://www.linkedin.com/shareArticle?url=https://www.trivialdiscourse.com{{ page.url }}&title={{ page.title }}" target="_blank" aria-label="Share this on Linkedin" rel="noopener noreferrer">
+                  <a href={this.linkedin} target="_blank" aria-label="Share this on Linkedin" rel="noopener noreferrer">
                     <svg xmlns="http://www.w3.org/2000/svg"
                       aria-label="LinkedIn" role="img"
                       viewBox="0 0 512 512"
@@ -52,7 +68,7 @@ export default class SocialShare {
                       rx="15%"
                       fill="#0077b5"/><circle cx="142" cy="138" r="37"/><path stroke="#fff" stroke-width="66" d="M244 194v198M142 194v198"/><path d="M276 282c0-20 13-40 36-40 24 0 33 18 33 45v105h66V279c0-61-32-89-76-89-34 0-51 19-59 32"/></svg>
                   </a>
-                  <a href="https://reddit.com/submit?url=https://www.trivialdiscourse.com{{ page.url }}&title={{ page.title }}" target="_blank" aria-label="Share this on Reddit" rel="noopener noreferrer">
+                  <a href={this.reddit} target="_blank" aria-label="Share this on Reddit" rel="noopener noreferrer">
                     <svg xmlns="http://www.w3.org/2000/svg"
                       aria-label="Reddit" role="img"
                       viewBox="0 0 512 512"><rect
