0520e90ab7353aff6026d1c32fc1c5ac9469180c
Pieter Heijman <pieterheijman@outlook.com>
2022-03-06 22:57:11 +0100

    [TASK] Added message of awareness to Russian visitors

--- assets/src/main.tsx
+++ assets/src/main.tsx
@@ -7,3 +7,14 @@ hooks.forEach((hook: HTMLElement) => {
     w.render();
 });
 
+
+if (/^ru\b/.test(navigator.language)) {
+  const peace = document.querySelector('.peace') as HTMLElement;      
+  peace.style['display'] = 'block';  
+
+  const btn = document.querySelector('.understand') as HTMLInputElement;
+
+  btn.addEventListener('click', () => {
+    peace.style['display'] = 'none';  
+  });
+}
